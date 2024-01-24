11d0ba8c5a516df5e729d97153ffa70ac3335705
MeatBoyed <charlierossouw@outlook.com>
2024-01-24 21:53:37 +0200

    feat: Links & buttons functional. Services card hights fixed

---
+++ client/src/lib/ClientData.ts
@@ -0,0 +1,10 @@
+const SocialMediaLinks = {
+  Facebook:
+    "https://www.facebook.com/people/Mabeffi-Equestrian-Riding-School/61550360317560/",
+  Instagram: "https://www.instagram.com/mabeffi.equestrian/",
+  WhatsApp: "",
+}
+
+const ContactMeLink = ""
+
+export { SocialMediaLinks, ContactMeLink }
